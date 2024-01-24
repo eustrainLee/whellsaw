ee06c36d92e71004aa28850f38e1016bc3475bba
eustrainLee <eustrain@outlook.com>
2024-01-24 20:25:37 +0800

    feat: add some code to test tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,12 +4,22 @@
 
 #[tauri::command]
 fn next_number(value: i32) -> i32 {
-  return value+2
+  return value+1
 }
+
+#[tauri::command]
+fn refresh(value: String, count: i32) -> (String, i32) {
+  if value.len() <= 9 {
+    return (value + "abc", count+1)
+  } else {
+    return (String::from(&value[0..3]),count+1)// String::from(&value[0..3])
+  }
+}
+
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![next_number])
+    .invoke_handler(tauri::generate_handler![next_number,refresh])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
